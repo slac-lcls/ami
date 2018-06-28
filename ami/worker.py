f8529a1becd0ae196c52759609363c007e5ace14
tjlane <thomas.joseph.lane@gmail.com>
2018-06-28 15:36:41 -0700

    flake8

--- ami/worker.py
+++ ami/worker.py
@@ -2,7 +2,6 @@ import re
 import os
 import sys
 import zmq
-import time
 import json
 import shutil
 import tempfile
@@ -10,11 +9,11 @@ import argparse
 import multiprocessing as mp
 from ami.graph import Graph, GraphConfigError, GraphRuntimeError
 from ami.comm import Ports, Collector, ResultStore, EventBuilder
-from ami.data import MsgTypes, Transitions, Message, Transition, StaticSource, Strategies
+from ami.data import MsgTypes, Transitions, Transition, StaticSource, Strategies
+
 
 class Worker(object):
     def __init__(self, idnum, src, collector_addr, graph_addr):
-
         """
         idnum : int
             a unique integer identifying this worker
@@ -38,7 +37,7 @@ class Worker(object):
         sources = []
         partition = self.src.partition()
         self.store.message(MsgTypes.Transition,
-                           self.idnum, 
+                           self.idnum,
                            Transition(Transitions.Allocate, partition))
         for name, dtype in partition:
             self.store.create(name, dtype)
@@ -56,19 +55,23 @@ class Worker(object):
                         if topic == "graph":
                             new_graph = payload
                         else:
-                            print("worker%d: No handler for received topic: %s"%(self.idnum, topic))
+                            print(
+                                "worker%d: No handler for received topic: %s" %
+                                (self.idnum, topic))
                     except zmq.Again:
                         break
                 if new_graph is not None:
 
                     self.graph.update(new_graph)
 
-                    print("worker%d: Received new configuration"%self.idnum)
+                    print("worker%d: Received new configuration" % self.idnum)
                     try:
                         self.graph.configure(sources)
-                        print("worker%d: Configuration complete"%self.idnum)
+                        print("worker%d: Configuration complete" % self.idnum)
                     except GraphConfigError as graph_err:
-                        print("worker%d: Configuration failed reverting to previous config:"%self.idnum, graph_err)
+                        print(
+                            "worker%d: Configuration failed reverting to previous config:" %
+                            self.idnum, graph_err)
                         # if this fails we just die
                         self.graph.revert()
 
@@ -77,14 +80,16 @@ class Worker(object):
                     self.store.message(MsgTypes.Graph, self.idnum, new_graph)
             elif msg.mtype == MsgTypes.Datagram:
                 # clear old values from the store
-                #self.store.clear()
+                # self.store.clear()
                 for dgram in msg.payload:
                     self.store.put(dgram.name, dgram.data)
 
                 try:
                     self.graph.execute()
                 except GraphRuntimeError as graph_err:
-                    print("worker%s: Failure encountered executing graph:"%self.idnum, graph_err)
+                    print(
+                        "worker%s: Failure encountered executing graph:" %
+                        self.idnum, graph_err)
                     return 1
             else:
                 self.store.send(msg)
@@ -95,7 +100,8 @@ class NodeCollector(Collector):
         super(__class__, self).__init__(collector_addr)
         self.node = node
         self.num_workers = num_workers
-        self.store = EventBuilder(self.num_workers, 10, downstream_addr, self.ctx)
+        self.store = EventBuilder(
+            self.num_workers, 10, downstream_addr, self.ctx)
         self.strategies = {}
 
     def process_msg(self, msg):
@@ -110,41 +116,65 @@ class NodeCollector(Collector):
         elif msg.mtype == MsgTypes.Datagram:
             if msg.payload.name in self.strategies:
                 if self.strategies[msg.payload.name] == Strategies.Sum.value:
-                    self.store.sum(msg.heartbeat, msg.identity, msg.payload.name, msg.payload.data, 0)
+                    self.store.sum(
+                        msg.heartbeat,
+                        msg.identity,
+                        msg.payload.name,
+                        msg.payload.data,
+                        0)
                 elif self.strategies[msg.payload.name] == Strategies.Avg.value:
-                    self.store.sum(msg.heartbeat, msg.identity, msg.payload.name, msg.payload.data, msg.payload.weight)
+                    self.store.sum(
+                        msg.heartbeat,
+                        msg.identity,
+                        msg.payload.name,
+                        msg.payload.data,
+                        msg.payload.weight)
                 elif self.strategies[msg.payload.name] == Strategies.Pick1.value:
-                    self.store.put(msg.heartbeat, msg.identity, msg.payload.name, msg.payload.data)
+                    self.store.put(
+                        msg.heartbeat,
+                        msg.identity,
+                        msg.payload.name,
+                        msg.payload.data)
                 else:
-                    print("node_collector%d: Unknown collector strategy - %s"%(self.node, self.strategies[msg.payload.name]))
+                    print("node_collector%d: Unknown collector strategy - %s" %
+                          (self.node, self.strategies[msg.payload.name]))
             else:
                 # We assume Pick1 for the stuff that comes from the raw data
-                self.store.put(msg.heartbeat, msg.identity, msg.payload.name, msg.payload.data)
+                self.store.put(
+                    msg.heartbeat,
+                    msg.identity,
+                    msg.payload.name,
+                    msg.payload.data)
         elif msg.mtype == MsgTypes.Graph:
             self.strategies = Graph.extract_collection_strategies(msg.payload)
 
+
 def run_worker(num, source, collector_addr, graph_addr):
 
-    print('Starting worker # %d, sending to collector at %s' % (num, collector_addr))
+    print(
+        'Starting worker # %d, sending to collector at %s' %
+        (num, collector_addr))
 
     if source[0] == 'static':
         try:
             with open(source[1], 'r') as cnf:
                 src_cfg = json.load(cnf)
         except OSError as os_exp:
-            print("worker%03d: problem opening json file:"%num, os_exp)
+            print("worker%03d: problem opening json file:" % num, os_exp)
             return 1
         except json.decoder.JSONDecodeError as json_exp:
-            print("worker%03d: problem parsing json file (%s):"%(num, source[1]), json_exp)
+            print(
+                "worker%03d: problem parsing json file (%s):" %
+                (num, source[1]), json_exp)
             return 1
 
-        src = StaticSource(num, 
-                           src_cfg['interval'], 
+        src = StaticSource(num,
+                           src_cfg['interval'],
                            src_cfg["init_time"],
                            src_cfg['heartbeat'],
                            src_cfg['config'])
     else:
-        print("worker%03d: unknown data source type:"%num, source[0])
+        print("worker%03d: unknown data source type:" % num, source[0])
         return 1
     worker = Worker(num, src, collector_addr, graph_addr)
     return worker.run()
@@ -152,7 +182,11 @@ def run_worker(num, source, collector_addr, graph_addr):
 
 def run_collector(node_num, num_workers, collector_addr, upstream_addr):
     print('Starting collector on node # %d' % node_num)
-    collector = NodeCollector(node_num, num_workers, collector_addr, upstream_addr)
+    collector = NodeCollector(
+        node_num,
+        num_workers,
+        collector_addr,
+        upstream_addr)
     return collector.run()
 
 
@@ -171,7 +205,7 @@ def main():
         '--port',
         type=int,
         default=Ports.Comm,
-        help='port for GUI-Manager communication (default: %d)'%Ports.Comm
+        help='port for GUI-Manager communication (default: %d)' % Ports.Comm
     )
 
     parser.add_argument(
@@ -179,7 +213,7 @@ def main():
         '--graph',
         type=int,
         default=Ports.Graph,
-        help='port for graph communication (default: %d)'%Ports.Graph
+        help='port for graph communication (default: %d)' % Ports.Graph
     )
 
     parser.add_argument(
@@ -187,7 +221,7 @@ def main():
         '--collector',
         type=int,
         default=Ports.Collector,
-        help='port for final collector (default: %d)'%Ports.Collector
+        help='port for final collector (default: %d)' % Ports.Collector
     )
 
     parser.add_argument(
@@ -214,10 +248,9 @@ def main():
 
     args = parser.parse_args()
     ipcdir = tempfile.mkdtemp()
-    collector_addr = "ipc://%s/node_collector"%ipcdir
-    upstream_addr = "tcp://%s:%d"%(args.host, args.collector)
-    graph_addr = "tcp://%s:%d"%(args.host, args.graph)
-    comm_addr = "tcp://%s:%d"%(args.host, args.port)
+    collector_addr = "ipc://%s/node_collector" % ipcdir
+    upstream_addr = "tcp://%s:%d" % (args.host, args.collector)
+    graph_addr = "tcp://%s:%d" % (args.host, args.graph)
 
     procs = []
     failed_worker = False
@@ -232,7 +265,7 @@ def main():
 
         for i in range(args.num_workers):
             proc = mp.Process(
-                name='worker%03d-n%03d'%(i, args.node_num),
+                name='worker%03d-n%03d' % (i, args.node_num),
                 target=run_worker,
                 args=(i, src_cfg, collector_addr, graph_addr)
             )
@@ -241,9 +274,13 @@ def main():
             procs.append(proc)
 
         collector_proc = mp.Process(
-            name='manager-n%03d'%args.node_num,
+            name='manager-n%03d' % args.node_num,
             target=run_collector,
-            args=(args.node_num, args.num_workers, collector_addr, upstream_addr)
+            args=(
+                args.node_num,
+                args.num_workers,
+                collector_addr,
+                upstream_addr)
         )
         collector_proc.daemon = True
         collector_proc.start()
@@ -252,10 +289,12 @@ def main():
         for proc in procs:
             proc.join()
             if proc.exitcode == 0:
-                print('%s exited successfully'%proc.name)
+                print('%s exited successfully' % proc.name)
             else:
                 failed_worker = True
-                print('%s exited with non-zero status code: %d'%(proc.name, proc.exitcode))
+                print(
+                    '%s exited with non-zero status code: %d' %
+                    (proc.name, proc.exitcode))
 
         # return a non-zero status code if any workerss died
         if failed_worker:
@@ -271,4 +310,4 @@ def main():
 
 
 if __name__ == '__main__':
-    sys.exit(main()) 
+    sys.exit(main())
