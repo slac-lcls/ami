9c6ff287a0c12f0ef59b40912d0e880d15715ceb
Daniel Damiani <ddamiani@slac.stanford.edu>
2018-07-05 17:00:59 -0700

    simple test that launches ami

---
+++ tests/test_local.py
@@ -0,0 +1,93 @@
+import os
+import re
+import sys
+import shutil
+import signal
+import tempfile
+import multiprocessing as mp
+
+from ami.comm import Ports
+from ami.manager import run_manager
+from ami.worker import run_worker, run_collector
+
+
+def test_local():
+    port = Ports.Comm
+    tcp = False
+    ipcdir = None
+    source = 'static://examples/worker.json'
+    num_workers = 2
+    heartbeat = 10
+    if tcp:
+        host = "127.0.0.1"
+        collector_addr = "tcp://%s:%d" % (host, port)
+        finalcol_addr = "tcp://%s:%d" % (host, port+1)
+        graph_addr = "tcp://%s:%d" % (host, port+2)
+        comm_addr = "tcp://%s:%d" % (host, port+3)
+    else:
+        ipcdir = tempfile.mkdtemp()
+        collector_addr = "ipc://%s/node_collector" % ipcdir
+        finalcol_addr = "ipc://%s/collector" % ipcdir
+        graph_addr = "ipc://%s/graph" % ipcdir
+        comm_addr = "ipc://%s/comm" % ipcdir
+
+    procs = []
+    failed_proc = False
+
+    try:
+        src_url_match = re.match('(?P<prot>.*)://(?P<body>.*)', source)
+        if src_url_match:
+            src_cfg = src_url_match.groups()
+        else:
+            print("Invalid data source config string:", source)
+            return 1
+
+        for i in range(num_workers):
+            proc = mp.Process(
+                name='worker%03d-n0' % i,
+                target=run_worker,
+                args=(i, num_workers, heartbeat, src_cfg, collector_addr, graph_addr)
+            )
+            proc.daemon = True
+            proc.start()
+            procs.append(proc)
+
+        collector_proc = mp.Process(
+            name='nodecol-n0',
+            target=run_collector,
+            args=(0, num_workers, collector_addr, finalcol_addr)
+        )
+        collector_proc.daemon = True
+        collector_proc.start()
+        procs.append(collector_proc)
+
+        manager_proc = mp.Process(
+            name='manager',
+            target=run_manager,
+            args=(finalcol_addr, graph_addr, comm_addr)
+        )
+        manager_proc.daemon = True
+        manager_proc.start()
+        procs.append(manager_proc)
+
+        # Do the tests
+
+        for proc in procs:
+            proc.terminate()
+            proc.join()
+            if proc.exitcode == 0 or proc.exitcode == -signal.SIGTERM:
+                print('%s exited successfully' % proc.name)
+            else:
+                failed_proc = True
+                print('%s exited with non-zero status code: %d' % (proc.name, proc.exitcode))
+
+        # return a non-zero status code if any workerss died
+        if failed_proc:
+            return 1
+
+    except KeyboardInterrupt:
+        print("Worker killed by user...")
+        return 0
+    finally:
+        if ipcdir is not None and os.path.exists(ipcdir):
+            shutil.rmtree(ipcdir)
