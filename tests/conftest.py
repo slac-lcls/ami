4630115e31a537416d3f2bdc9d9fa9fcd278b0fb
Daniel Damiani <ddamiani@slac.stanford.edu>
2019-01-18 18:20:36 -0800

    dill files aren't portable to different python versions so create the complex graph dill once for each session

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+import dill
+import numpy as np
+
+from ami.graphkit_wrapper import Graph, Map, FilterOn, FilterOff, Binning
+
+
+@pytest.fixture(scope='module')
+def complex_graph(tmpdir_factory):
+    graph = Graph(name='graph')
+
+    def roi(cspad):
+        return cspad[:100, :100]
+
+    graph.add(Map(name='Roi', inputs=['cspad'], outputs=['roi'], func=roi))
+    graph.add(Map(name='Sum', inputs=['roi'], outputs=['sum'], func=np.sum))
+
+    graph.add(FilterOn(name='FilterOn', condition_needs=['laser'], outputs=['laseron']))
+    graph.add(Binning(name='BinningOn', condition_needs=['laseron'], inputs=['delta_t', 'sum'],
+                      outputs=['signal']))
+
+    graph.add(FilterOff(name='FilterOff', condition_needs=['laser'], outputs=['laseroff']))
+    graph.add(Binning(name='BinningOff', condition_needs=['laseroff'],
+                      inputs=['delta_t', 'sum'], outputs=['reference']))
+
+    fname = tmpdir_factory.mktemp("graphs", False).join("complex_graph.dill")
+
+    with open(fname, 'wb') as fd:
+        dill.dump(graph, fd)
+    return fname
--- tests/test_local.py
+++ tests/test_local.py
@@ -18,39 +18,41 @@ from ami.comm import Ports, GraphCommHandler
 from ami.local import build_parser, run_ami
 
 
+@pytest.fixture(scope='class')
+def start_ami(pytestconfig):
+    parser = build_parser()
+    args = parser.parse_args(["-n", "1", '-t', '--headless', 'static://%s' % os.path.join(pytestconfig.rootdir, 'tests', 'worker_test.json') ])
+
+    queue = mp.Queue()
+    ami = mp.Process(name='ami',
+                     target=run_ami,
+                     args=(args, queue))
+    ami.start()
+
+    host = "127.0.0.1"
+    comm_addr = "tcp://%s:%d" % (host, Ports.Comm+3)
+    comm_handler = GraphCommHandler(comm_addr)
+
+    yield (comm_handler, pytestconfig.rootdir)
+
+    queue.put(None)
+    ami.join()
+
+    if ami.exitcode == 0 or ami.exitcode == -signal.SIGTERM:
+        print('AMI exited successfully')
+    else:
+        print('AMI exited with non-zero status code: %d' % ami.exitcode)
+        return 1
+
+    return 0
+
+
+@pytest.mark.usefixtures("start_ami")
 class TestAMI(object):
 
-    @pytest.fixture(scope='class')
-    def start_ami(self, pytestconfig):
-        parser = build_parser()
-        args = parser.parse_args(["-n", "1", '-t', '--headless', 'static://worker.json'])
-
-        queue = mp.Queue()
-        ami = mp.Process(name='ami',
-                              target=run_ami,
-                              args=(args, queue))
-        ami.start()
-
-        host = "127.0.0.1"
-        comm_addr = "tcp://%s:%d" % (host, Ports.Comm+3)
-        comm_handler = GraphCommHandler(comm_addr)
-
-        yield (comm_handler, pytestconfig.rootdir)
-
-        queue.put(None)
-        ami.join()
-
-        if ami.exitcode == 0 or ami.exitcode == -signal.SIGTERM:
-            print('AMI exited successfully')
-        else:
-            print('AMI exited with non-zero status code: %d' % ami.exitcode)
-            return 1
-
-        return 0
-
-    def test_complex_graph(self, start_ami):
+    def test_complex_graph(self, complex_graph, start_ami):
         comm_handler, root_dir = start_ami
-        comm_handler.load(os.path.join(root_dir, 'tests', 'complex_graph.dill'))
+        comm_handler.load(complex_graph)
         start = time.time()
         while comm_handler.graphVersion != comm_handler.featuresVersion:
             end = time.time()
