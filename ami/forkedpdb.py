a78335a7c45f3c0804b51546a830034d28865022
Seshu Yamajala <syamajala@gmail.com>
2019-02-25 16:48:24 -0800

    Add support for Qt to ForkedPdb.

--- ami/forkedpdb.py
+++ ami/forkedpdb.py
@@ -1,6 +1,7 @@
 # ref: http://stackoverflow.com/questions/4716533/how-to-attach-debugger-to-a-python-subproccess
 import sys
 import pdb
+import PyQt5
 
 
 class ForkedPdb(pdb.Pdb):
@@ -11,9 +12,11 @@ class ForkedPdb(pdb.Pdb):
         _stdin = sys.stdin
         try:
             sys.stdin = open('/dev/stdin')
+            PyQt5.QtCore.pyqtRemoveInputHook()
             pdb.Pdb.interaction(self, *args, **kwargs)
         finally:
             sys.stdin = _stdin
+            PyQt5.QtCore.pyqtRestoreInputHook()
 
 # Use it the same way you might use the classic Pdb:
 # ForkedPdb().set_trace()
