3777dfd043b488c608409fd6bc65b09f5a958b92
Daniel Damiani <ddamiani@slac.stanford.edu>
2019-12-19 13:26:24 -0800

    fix build all script

--- build_all.sh
+++ build_all.sh
@@ -9,53 +9,14 @@ else
   export INSTDIR="$TESTRELDIR"
 fi
 
-cmake_option="Debug"
 pyInstallStyle="develop"
-psana_setup_args=""
 
-while getopts ":c:p:s:" opt; do
-  case $opt in
-    c) cmake_option="$OPTARG"
-    ;;
-    p) pyInstallStyle="$OPTARG"
-    ;;
-    s) psana_setup_args="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    ;;
-  esac
-done
-
-echo "CMAKE_BUILD_TYPE:" $cmake_option
 echo "Python install option:" $pyInstallStyle
-
-
-function cmake_build() {
-    cd $1
-    mkdir -p build
-    cd build
-    cmake -DCMAKE_INSTALL_PREFIX=$INSTDIR -DCMAKE_BUILD_TYPE=$cmake_option ..
-    make -j 4 install
-    cd ../..
-}
-
-#cmake_build xtcdata
-#cmake_build psdaq
-#cmake_build psalg
 
 pyver=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
 # "python setup.py develop" seems to not create this for you
 # (although "install" does)
 mkdir -p $INSTDIR/lib/python$pyver/site-packages/
 
-# to build psana with setuptools
-#cd psana
-# force build of the extensions.  do this because in some cases
-# setup.py is unable to detect if an external header file changed
-# (e.g. in xtcdata).  but in many cases it is fine without "-f" - cpo
-#python setup.py build_ext --xtcdata=$INSTDIR -f --inplace
-#python setup.py $pyInstallStyle $psana_setup_args --xtcdata=$INSTDIR --prefix=$INSTDIR
-#cd ..
-# build ami
-#cd ami
-python setup.py $pyInstallStyle --prefix=$INSTDIR
+# to build ami with setuptools
+python setup.py $pyInstallStyle --no-deps --prefix=$INSTDIR
