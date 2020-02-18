48ce5c1fa11b09ad28875ad9d95e4e538aa20f8c
Seshu Yamajala <syamajala@gmail.com>
2020-02-18 13:44:08 -0800

    Add scatter roi.

--- ami/flowchart/library/Roi.py
+++ ami/flowchart/library/Roi.py
@@ -1,8 +1,9 @@
-from ami.flowchart.library.DisplayWidgets import AreaDetWidget, WaveformWidget, PixelDetWidget
+from ami.flowchart.library.DisplayWidgets import AreaDetWidget, WaveformWidget, PixelDetWidget, ScatterWidget
 from ami.flowchart.library.common import CtrlNode, MAX
 from amitypes import Array2d, Array1d
-import pyqtgraph as pg
 import ami.graph_nodes as gn
+import pyqtgraph as pg
+import numpy as np
 import asyncio
 
 
@@ -150,6 +151,105 @@ class Roi1D(CtrlNode):
                       func=self.func,
                       parent=self.name())
         return node
+
+
+class ScatterRoi(CtrlNode):
+
+    """
+    Region of Interest of 1d array.
+    """
+
+    nodeName = "ScatterRoi"
+    uiTemplate = [('origin',  'intSpin', {'value': 0, 'min': 0, 'max': MAX}),
+                  ('extent',  'intSpin', {'value': 10, 'min': 1, 'max': MAX}),
+                  ('Num Points', 'intSpin', {'value': 100, 'min': 1, 'max': MAX})]
+
+    def __init__(self, name):
+        super().__init__(name, terminals={"X": {"io": "in", "ttype": float},
+                                          "Y": {"io": "in", "ttype": float},
+                                          "Out.X": {"io": "out", "ttype": Array1d},
+                                          "Out.Y": {"io": "out", "ttype": Array1d}},
+                         buffered=True)
+
+        def func(arr):
+            x, y = zip(*arr)
+            return np.array(x), np.array(y)
+
+        self.func = func
+
+    def display(self, topics, terms, addr, win, **kwargs):
+        if self.widget is None:
+            self.widget = ScatterWidget(topics, terms, addr, win, **kwargs)
+            self.widget.roi = pg.LinearRegionItem((0, 10))
+            self.widget.roi.setBounds((0, None))
+            self.widget.plot_view.addItem(self.widget.roi)
+            self.widget.roi.sigRegionChangeFinished.connect(self.set_values)
+        if self.task is None and self.widget:
+            self.task = asyncio.ensure_future(self.widget.update())
+
+        return self.widget
+
+    def set_values(self, *args, **kwargs):
+        # need to block signals to the stateGroup otherwise stateGroup.sigChanged
+        # will be emmitted by setValue causing update to be called
+        self.stateGroup.blockSignals(True)
+        roi = args[0]
+        origin, extent = roi.getRegion()
+        self.origin = int(origin)
+        self.extent = int(extent)
+        self.ctrls['origin'].setValue(self.origin)
+        self.ctrls['extent'].setValue(self.extent)
+        self.set_func()
+        self.stateGroup.blockSignals(False)
+        self.sigStateChanged.emit(self)
+
+    def update(self, *args, **kwargs):
+        self.origin = self.ctrls['origin'].value()
+        self.extent = self.ctrls['extent'].value()
+        self.set_func()
+        if self.widget:
+            self.widget.roi.setRegion((self.origin, self.extent))
+
+    def set_func(self):
+        origin = self.origin
+        extent = self.extent
+
+        def func(arr):
+            arr = np.array(arr)
+
+            roi = arr[(origin < arr[:, 0]) & (arr[:, 0] < extent)]
+            if roi.size > 0:
+                return roi[:, 0], roi[:, 1]
+            else:
+                return np.array([]), np.array([])
+
+        self.func = func
+
+    def buffered_topics(self):
+        terms = self.input_vars()
+        return {terms["X"]: self.name()+"_displayX", terms["Y"]: self.name()+"_displayY"}
+
+    def buffered_terms(self):
+        terms = self.input_vars()
+        return {"X": terms["X"], "Y": terms["Y"]}
+
+    def to_operation(self, inputs, conditions={}):
+        outputs = self.output_vars()
+        pickn_outputs = [self.name()+"_picked"]
+        display_outputs = [self.name()+"_displayX", self.name()+"_displayY"]
+
+        def display_func(arr):
+            x, y = zip(*arr)
+            return np.array(x), np.array(y)
+
+        nodes = [gn.PickN(name=self.name()+"_pickN", condition_needs=list(conditions.values()),
+                          inputs=list(inputs.values()), outputs=pickn_outputs, parent=self.name(), N=self.Num_Points),
+                 gn.Map(name=self.name()+"_operation", inputs=pickn_outputs, outputs=outputs, func=self.func,
+                        parent=self.name()),
+                 gn.Map(name=self.name()+"_display", inputs=pickn_outputs, outputs=display_outputs, parent=self.name(),
+                        func=display_func)]
+
+        return nodes
 
 
 class Roi0D(CtrlNode):
