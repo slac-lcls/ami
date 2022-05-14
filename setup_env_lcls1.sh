c7489f49f0edf5c7905d6ee1e2da9f4cb41faab1
Daniel Damiani <ddamiani@slac.stanford.edu>
2022-05-14 15:04:50 -0700

    lcls1 setup_env installs in a different dir than lcls2

--- setup_env_lcls1.sh
+++ setup_env_lcls1.sh
@@ -3,10 +3,10 @@ unset PYTHONPATH
 # Setup standard py3 version of the ana conda env
 source /reg/g/psdm/etc/psconda.sh -py3
 RELDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PATH=$RELDIR/install/bin:${PATH}
+export PATH=$RELDIR/install-lcls1/bin:${PATH}
 pyver=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
-export PYTHONPATH=$RELDIR/install/lib/python$pyver/site-packages
+export PYTHONPATH=$RELDIR/install-lcls1/lib/python$pyver/site-packages
 # for procmgr
-export TESTRELDIR=$RELDIR/install
+export TESTRELDIR=$RELDIR/install-lcls1
 # needed so mypy finds type stubs in local dir
 export MYPYPATH=$PYTHONPATH
