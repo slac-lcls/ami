930f9b4ab6b720f292e99ef335144e125ed464a7
Daniel Damiani <ddamiani@slac.stanford.edu>
2022-05-17 14:02:18 -0700

    add more lcls1 bld support

--- ami/psana/bld.py
+++ ami/psana/bld.py
@@ -1,9 +1,25 @@
 import psana
+import amitypes
 from .detector import BldMeta, DdlHelperMeta, DdlHelper, Detector
 from .utils import export
 
 
 __all__ = []
+
+
+def _get_bld_names():
+    names = []
+    try:
+        for i in range(256):
+            names.append(psana.BldInfo(i).typeName())
+    except ValueError:
+        pass
+
+    return names
+
+
+BLD_NAMES = _get_bld_names()
+AIN_BLD_NAMES = [name for name in BLD_NAMES if "-AIN-" in name]
 
 
 class DdlEBeam(DdlHelper,
@@ -13,24 +29,60 @@ class DdlEBeam(DdlHelper,
 
 
 @export
-class EBeam(Detector,
-            metaclass=BldMeta,
-            detcls=DdlEBeam,
-            sources=['EBeam'],
-            annotations=psana.Bld.BldDataEBeam):
+class EBeamDetector(Detector,
+                    metaclass=BldMeta,
+                    detcls=DdlEBeam,
+                    sources=['EBeam'],
+                    annotations=psana.Bld.BldDataEBeam):
     pass
 
 
 class DdlFEEGasDet(DdlHelper,
                    metaclass=DdlHelperMeta,
-                   methods=[psana.Bld.BldDataFEEGasDetEnergyV1]):
+                   methods=psana.Bld.BldDataFEEGasDetEnergyV1):
     pass
 
 
 @export
-class FEEGasDet(Detector,
-                metaclass=BldMeta,
-                detcls=DdlFEEGasDet,
-                sources=['FEEGasDetEnergy'],
-                annotations=[psana.Bld.BldDataFEEGasDetEnergyV1]):
+class FEEGasDetDetector(Detector,
+                        metaclass=BldMeta,
+                        detcls=DdlFEEGasDet,
+                        sources=['FEEGasDetEnergy'],
+                        annotations=psana.Bld.BldDataFEEGasDetEnergyV1):
     pass
+
+
+class DdlPhaseCavity(DdlHelper,
+                     metaclass=DdlHelperMeta,
+                     methods=psana.Bld.BldDataPhaseCavityV1):
+    pass
+
+
+@export
+class PhaseCavityDetector(Detector,
+                          metaclass=BldMeta,
+                          detcls=DdlPhaseCavity,
+                          sources=['PhaseCavity'],
+                          annotations=psana.Bld.BldDataPhaseCavityV1):
+    pass
+
+
+class DdlAnalogInput(DdlHelper,
+                     metaclass=DdlHelperMeta,
+                     methods=psana.Bld.BldDataAnalogInput):
+    pass
+
+
+@export
+class AnalogInputDetector(Detector,
+                          metaclass=BldMeta,
+                          detcls=DdlAnalogInput,
+                          sources=AIN_BLD_NAMES,
+                          annotations=psana.Bld.BldDataAnalogInput,
+                          overrides={'channelVoltages': amitypes.MultiChannelFloat}):
+    def __init__(self, src, env):
+        super().__init__(src, env)
+
+    @property
+    def nchannels(self):
+        return 16
