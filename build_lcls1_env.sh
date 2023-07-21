f984a2f4fabb4ec3c54e7c5abc65a4178518fc27
Daniel Damiani <ddamiani@slac.stanford.edu>
2023-07-21 05:32:16 -0700

    lcls1 env build script has the build directory as option instead of hardcoded

--- build_lcls1_env.sh
+++ build_lcls1_env.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+
+BUILDDIR=${1:-$PWD}
+
 pip install --prefix=${TESTRELDIR} asyncqt mypy setproctitle pyfftw sympy pint pytest-asyncio pytest-qt p4p==3.5.5
 pip install --prefix=${TESTRELDIR} pyflakes autopep8 docutils pycodestyle pathspec qtawesome
-cd /cds/home/d/ddamiani/Workarea/lcls2-dev/networkfox
+cd "${BUILDDIR}/networkfox"
 pip install --no-deps --prefix=${TESTRELDIR} .
-cd /cds/home/d/ddamiani/Workarea/lcls2-dev/amityping
+cd "${BUILDDIR}/amityping"
 pip install --no-deps --prefix=${TESTRELDIR} .
-cd /cds/home/d/ddamiani/Workarea/lcls2-dev/pyqode.qt
+cd "${BUILDDIR}/pyqode.qt"
 pip install --no-deps --prefix=${TESTRELDIR} .
-cd /cds/home/d/ddamiani/Workarea/lcls2-dev/pyqode.python
+cd "${BUILDDIR}/pyqode.python"
 pip install --no-deps --prefix=${TESTRELDIR} .
-cd /cds/home/d/ddamiani/Workarea/lcls2-dev/pyqode.core
+cd "${BUILDDIR}/pyqode.core"
 pip install --no-deps --prefix=${TESTRELDIR} .
