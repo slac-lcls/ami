845b60274ceef5edbf470d511f77b18fa10e7770
Seshu Yamajala <syamajala@gmail.com>
2024-09-19 03:51:53 -0400

    Clean up export boxes.

--- ami/flowchart/library/Export.py
+++ ami/flowchart/library/Export.py
@@ -5,18 +5,32 @@ from ami.flowchart.library.common import CtrlNode
 import ami.graph_nodes as gn
 
 
-class Export(CtrlNode):
+class ExportToWorker(CtrlNode):
 
     """
-    Send data back to worker.
+    Send data back to worker from global collector.
     """
 
-    nodeName = "Export"
-    uiTemplate = [('alias', 'text')]
+    nodeName = "ExportToWorker"
+    uiTemplate = [('alias', 'text', {'tip': "Name to export data under."})]
 
     def __init__(self, name):
         super().__init__(name, terminals={"In": {'io': 'in', 'ttype': Any},
                                           "Out": {'io': 'out', 'ttype': Any}},
+                         exportable=True)
+
+
+class PvExport(CtrlNode):
+
+    """
+    Export data through an AMI hosted PV using either PV access or channel access.
+    """
+
+    nodeName = "PvExport"
+    uiTemplate = [('alias', 'text', {'tip': "PV name to export variable under."})]
+
+    def __init__(self, name):
+        super().__init__(name, terminals={"In": {'io': 'in', 'ttype': Any}},
                          exportable=True)
 
 
@@ -58,27 +72,30 @@ class ZMQ(CtrlNode):
         return super().display(topics, terms, addr, win, ZMQWidget, **kwargs)
 
 try:
-    import epics
+    import caproto.threading.client as ct
 
     class CaputProc():
         def __init__(self, pvname):
             self.pvname = pvname
+            self.ctx = None
             self.pv = None
 
         def __call__(self, value):
-            if self.pv is None:
-                self.pv = epics.PV(self.pvname)
-            return self.pv.put(value)
+            if self.ctx is None:
+                self.ctx = ct.Context()
+                self.pv = self.ctx.get_pvs(self.pvname)[0]
+            return self.pv.write(value)
 
     class Caput(CtrlNode):
 
         """
-        Send data to a PV via Channel Access.
+        Send data to an existing externally hosted PV via Channel Access.
         """
 
         nodeName = "Caput"
         uiTemplate = [('pvname', 'text'),
-                      ('global', 'check', {'checked': True})]
+                      ('global', 'check', {'checked': True,
+                                           'tip': "Insert Pick1 and export values from global collector."})]
 
         def __init__(self, name):
             super().__init__(name, terminals={"In": {'io': 'in', 'ttype': Union[str, int, float, Array1d]}})
@@ -119,16 +136,17 @@ try:
     class Pvput(CtrlNode):
 
         """
-        Send data to a PV via PVAccess.
+        Send data to an existing externally hosted PV via PVAccess.
         """
 
         nodeName = "Pvput"
         uiTemplate = [('pvname', 'text'),
-                      ('global', 'check', {'checked': True})]
+                      ('global', 'check', {'checked': True,
+                                           'tip': "Insert Pick1 and export values from global collector."})]
 
         def __init__(self, name):
-            super().__init__(name, terminals={"In":
-                                              {'io': 'in', 'ttype': Union[str, int, float, Array1d, Array2d]}})
+            super().__init__(name,
+                             terminals={"In": {'io': 'in', 'ttype': Union[str, int, float, Array1d, Array2d]}})
 
         def to_operation(self, inputs, outputs, **kwargs):
             outputs = [self.name()+"_unused"]
@@ -147,7 +165,6 @@ try:
                                 func=PvputProc(self.values['pvname']), **kwargs)]
 
             return nodes
-
 
 except ImportError as e:
     print(e)
