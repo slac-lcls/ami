a249cea64e24545324495dcc03e956c86d2b5102
Seshu Yamajala <syamajala@gmail.com>
2026-04-07 21:15:11 -0700

    Add Black/isort configuration and pre-commit hooks
    - Add Black config to pyproject.toml (120 char, Python 3.9)
    - Add isort config to pyproject.toml (Black-compatible profile)
    - Update .flake8 for Black compatibility (E203, W503)
    - Add .pre-commit-config.yaml with formatting hooks
    - Add 'dev' extras to setup.py for formatting tools

--- setup.py
+++ setup.py
@@ -3,82 +3,82 @@ from setuptools import setup, find_packages
 
 
 def get_version(pkg):
-    version = '2.0.0'
+    version = "2.0.0"
     for arg in sys.argv:
-        if arg.startswith('--version'):
-            version = arg.split('=')[1]
+        if arg.startswith("--version"):
+            version = arg.split("=")[1]
             sys.argv.remove(arg)
 
     return version
 
 
 setup(
-    name='ami',
-    version=get_version('ami'),
-    description='LCLS analysis monitoring',
-    long_description='The package used at LCLS-II for online analysis monitoring',
-    author='Daniel Damiani',
-    author_email='ddamiani@slac.stanford.edu',
-    url='https://confluence.slac.stanford.edu/display/PSDMInternal/AMI+Replacement',
+    name="ami",
+    version=get_version("ami"),
+    description="LCLS analysis monitoring",
+    long_description="The package used at LCLS-II for online analysis monitoring",
+    author="Daniel Damiani",
+    author_email="ddamiani@slac.stanford.edu",
+    url="https://confluence.slac.stanford.edu/display/PSDMInternal/AMI+Replacement",
     packages=find_packages(),
-    setup_requires=[
-        'pytest-runner'
-    ],
+    setup_requires=["pytest-runner"],
     install_requires=[
-        'dill',
-        'pyzmq',
-        'numpy',
-        'pyqtgraph',
-        'networkfox',
-        'ipython',
-        'qtpy',
-        'qasync>=0.27.1',
-        'amityping>=1.1.2',
-        'mypy',
-        'setproctitle',
-        'prometheus_client',
-#        'lark'
+        "dill",
+        "pyzmq",
+        "numpy",
+        "pyqtgraph",
+        "networkfox",
+        "ipython",
+        "qtpy",
+        "qasync>=0.27.1",
+        "amityping>=1.1.2",
+        "mypy",
+        "setproctitle",
+        "prometheus_client",
+        #        'lark'
     ],
-    tests_require=[
-        'pytest',
-        'pytest-asyncio',
-        'pytest-qt'
-    ],
+    tests_require=["pytest", "pytest-asyncio", "pytest-qt"],
     extras_require={
-        'pva': ['p4p'],
-        'hdf5': ['h5py'],
-        'arrow': ['pyarrow>=0.17'],
-        'lcls': ['psana', 'h5py', 'p4p'],
-        'console': ['qtconsole']
+        "pva": ["p4p"],
+        "hdf5": ["h5py"],
+        "arrow": ["pyarrow>=0.17"],
+        "lcls": ["psana", "h5py", "p4p"],
+        "console": ["qtconsole"],
+        "dev": [
+            "pre-commit>=3.6.0",
+            "black==24.2.0",
+            "isort==5.13.2",
+            "flake8",
+            "flake8-black",
+        ],
     },
     entry_points={
-        'console_scripts': [
-            'ami-worker = ami.worker:main',
-            'ami-manager = ami.manager:main',
-            'ami-node = ami.collector:node_main',
-            'ami-global = ami.collector:global_main',
-            'ami-client = ami.client:main',
-            'ami-console = ami.console:main',
-            'ami-local = ami.local:main',
-            'ami-remote = ami.remote:main',
-            'ami-export = ami.export:main',
-            'ami-syncer = ami.sync:main',
-            'ami-monitor = ami.monitor:main'
+        "console_scripts": [
+            "ami-worker = ami.worker:main",
+            "ami-manager = ami.manager:main",
+            "ami-node = ami.collector:node_main",
+            "ami-global = ami.collector:global_main",
+            "ami-client = ami.client:main",
+            "ami-console = ami.console:main",
+            "ami-local = ami.local:main",
+            "ami-remote = ami.remote:main",
+            "ami-export = ami.export:main",
+            "ami-syncer = ami.sync:main",
+            "ami-monitor = ami.monitor:main",
         ]
     },
-    scripts=['ami/ami-mpi'],
+    scripts=["ami/ami-mpi"],
     classifiers=[
-        'Development Status :: 1 - Planning'
-        'Environment :: Other Environment',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Scientific/Engineering :: Visualization',
-        'Topic :: Utilities',
+        "Development Status :: 1 - PlanningEnvironment :: Other Environment",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Utilities",
     ],
 )
